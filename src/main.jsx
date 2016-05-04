08ed75f4fdcd3fea915d6975b66868b2f3c8a49a
Chase McElroy <ichasem@gmail.com>
2016-05-04 15:21:08 -0700

    Simple JS Ingredients Website
    created with React JS

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ListManager = require('./components/ListManager.jsx');
+
+ReactDOM.render(<ListManager title="ingredients" />, document.getElementById('ingredients'));
